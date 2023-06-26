4735ba9e0e1e63b39d6e64dec244da7cd032a731
Abhilekh Borah <abhilekhkey@gmail.com>
2023-06-26 20:45:07 +0530

    Initial Commit

---
+++ Watch(deltae)/Watch_deltae_App.swift
@@ -0,0 +1,17 @@
+//
+//  Watch_deltae_App.swift
+//  Watch(deltae)
+//
+//  Created by Abhilekh Borah on 26/06/23.
+//
+
+import SwiftUI
+
+@main
+struct Watch_deltae_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
